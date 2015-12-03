9e75725e3500f114bc1b86d58c3471f793c35c05
CodeMySky <codemysky@hotmail.com>
2015-12-03 15:10:30 -0500

    now can retrive personal tweets

--- codes/get_tweets_from_twitter.py
+++ codes/get_tweets_from_twitter.py
@@ -1,17 +1,16 @@
 import os
 import tweepy
 import time
-import tweepy.TweepError
+from tweepy import TweepError
 
 auth = tweepy.OAuthHandler("ltmdE1M5bgvzKgOrtzrXpBBLU", "4GqOV4zS0LmgrXuu2YE4sCaPgiUnC5kFBY2bM3y1utRkDbuHnZ")
 auth.set_access_token("2972025861-MaDigIstH835Ottkqk28pQynSshbyeBOtmd6R9M", "tzIKeulAqcXW9azRBF0E6OcwhXW7EcthO19EVQqrTpmhi")
 
 class MyModelParser(tweepy.parsers.ModelParser):
     def parse(self, method, payload):
-        # result = super(tweepy.parsers.ModelParser, self).parse(method, payload)
+        result = super(tweepy.parsers.ModelParser, self).parse(method, payload)
         # result._payload = payload
-        # print payload
-        return payload
+        return (result, payload)
 
 api = tweepy.API(auth, parser=MyModelParser())
 
@@ -23,15 +22,15 @@ def get_tweets(id_list):
     except TweepError as e:
         print e.response.status
     return tweets+"\n"
-        
 
-if __name__ == '__main__':
+
+def batch_retrive_tweets_by_id():
     count = 0
     for i in os.listdir(os.getcwd() + '/tweets'):
         print len(i)
         if len(i) == 32:
-            txt = open('./tweets/'+i, 'r')
-            out = open('./tweets/'+i+".json", 'w')
+            txt = open('../tweets/'+i, 'r')
+            out = open('../tweets/'+i+".json", 'w')
             tweet_id_list = []
             for tweet_id in txt:
                 if len(tweet_id) < 10:
@@ -47,3 +46,30 @@ if __name__ == '__main__':
             result_string = get_tweets(tweet_id_list)
             out.write(result_string.encode("utf8"))
     print count
+
+
+def retrive_single_batch_by_screen_name(screen_name, max_id=None):
+    try:
+        tweets, payload = api.user_timeline(screen_name, count=200, max_id=max_id)
+    except TweepError as e:
+        print e.response.status
+    return tweets, payload
+
+
+def retrive_tweets_by_screen_name(screen_name):
+    out = open("../data/{}.json".format(screen_name), 'w')
+    print "Retriving tweets for {}".format(screen_name)
+    count = 0
+    tweets, payload = retrive_single_batch_by_screen_name(screen_name)
+    print count
+    while len(tweets) == 200:
+        tweets, payload = retrive_single_batch_by_screen_name(screen_name, tweets[-1]["id"])
+        out.write(payload.encode("utf8"))
+        out.write("\n")
+        print count
+        print tweets[-1]["id"]
+        count += len(tweets)
+    out.close()
+
+if __name__ == '__main__':
+    retrive_tweets_by_screen_name("realDonaldTrump")
