9bb1f0792e0940f59d100bbb4ec9a0d1cb5c4fe1
guanw <lonelyjohner@163.com>
2015-12-07 14:34:45 -0500

    add code for preprocess different train and test data

--- codes/modules/config.py
+++ codes/modules/config.py
@@ -1,7 +1,8 @@
-background_dictionary_filename = 'background_dictionary.pkl'
-time_interval_filename = "time_interval.pkl"
-pos_tag_filename = "pos_tag.pkl"
-model_filename = "background_model.txt"
-hotwords_file = "hotwords.pkl"
-generate_groupburst_file = "groupburst.pkl"
-word_to_set_file = "word_to_set.pkl"
+background_dictionary_filename = 'background_dictionary2.pkl'
+time_interval_filename = "time_interval2.pkl"
+# pos_tag_filename = "pos_tag.pkl"
+model_filename = "background_model2.txt"
+hotwords_file = "hotwords2.pkl"
+generate_groupburst_file = "groupburst2.pkl"
+word_to_set_file = "word_to_set2.pkl"
+
--- codes/pipeline.py
+++ codes/pipeline.py
@@ -7,10 +7,12 @@ from modules.id_to_tweets import *
 from modules.personal import trend
 
 
-train_time_file = "train_text_time_en.csv"
+# train_time_file = "train_text_time_en.csv"
+train_time_file = "train_en_file2.csv"
 train_save_model(train_time_file)
 
-test_time_file = "test_sorted_tweets_en.csv"
+# test_time_file = "test_sorted_tweets_en.csv"
+test_time_file = "test_en_file2.csv"
 test_model(test_time_file, threshold=10)
 
 generate_hotword_to_tweet_dictionary(tweet_test_file = test_time_file, hotword_file = hotwords_file, generate_file = word_to_set_file)
@@ -18,7 +20,7 @@ group_burst(pickle_file = word_to_set_file, generate_file=generate_groupburst_fi
 
 id_to_tweets(test_time_file)
 #
-construct_test_file_matrix('test_sorted_tweets_en.csv')
+construct_test_file_matrix(test_time_file)
 a = pickle.load(open('./file/word_dictionary_list.pkl','rb'))
 print a[0]
 b = pickle.load(open('./file/trend_matrix.pkl','rb'))
